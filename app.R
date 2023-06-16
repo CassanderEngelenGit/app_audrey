6ce720c3322dad99dd0fde3c8c3fd2aa5e7451cf
Cassander Engelen <cassander.engelen@wur.nl>
2023-06-16 14:53:50 +0200

    Combines the selected and gives a combined code

--- app.R
+++ app.R
@@ -13,7 +13,8 @@ test.data <- tibble(program = c(1,1,1,2,2,3,3,3,3,3), type = c("A", "B", "B", "C
                     vigour = c(4,7,8,8,5,6,7,9,10,3), 
                     shape = c("Round", "Square", "Round","Round", "Square", 
                               "Round","Round", "Square", "Round","Square"), 
-                    pollen_quality = c(4,7,8,8,5,6,7,9,10,3))
+                    pollen_quality = c(4,7,8,8,5,6,7,9,10,3),
+                    code = c("xx2", "xy3", "xx4", "xy6", "yx7", "kw7", "ix6", "uw5", "lw7", "yd7"))
 
 
 ######################
@@ -61,14 +62,34 @@ ui <- dashboardPage(
 ############################################
 server <- function(input, output, session) {
   dat <- reactive({test.data})
+  
   dat2 <- reactive({
+    dat() %>% 
+      subset(program == dat()[[input$data_rows_selected, 1]] &
+               type == dat()[[input$data_rows_selected, 2]])
+  })
+  
+  dat3 <- reactive({
     dat()[input$data_rows_selected,] %>% 
-      bind_rows(dat()[input$data2_rows_selected,]) 
+      bind_rows(dat()[input$data2_rows_selected,]) %>% 
+      bind_rows(tibble(program = dat()[[input$data_rows_selected, 1]], 
+                       
+                       type = dat()[[input$data_rows_selected, 2]],
+                       
+                       vigour = ((dat()[[input$data_rows_selected, 3]]+dat()[[input$data2_rows_selected, 3]])/2),
+                       
+                       shape = paste(dat()[[input$data_rows_selected, 4]], "-", dat()[[input$data2_rows_selected, 4]]),
+                       
+                       pollen_quality = ((dat()[[input$data_rows_selected, 5]]+dat()[[input$data2_rows_selected, 5]])/2),
+                       
+                       code = paste(dat()[[input$data_rows_selected, 6]], "-", dat()[[input$data2_rows_selected, 6]])
+                       )
+                       )
   })
   
   output$data <-  DT::renderDataTable(dat(), selection = "single")
-  output$data2 <-  DT::renderDataTable(dat(), selection = "single")
-  output$data3 <-  DT::renderDataTable(dat2(), selection = "single")
+  output$data2 <-  DT::renderDataTable(dat2(), selection = "single")
+  output$data3 <-  DT::renderDataTable(dat3(), selection = "single")
   
 }
 
